a4ffcae175b7d0d5f4b4e2bc766250c2781d54b2
DarMcB <139492425+DarMcB@users.noreply.github.com>
2024-09-28 13:44:19 +0100

    replaceString function [DRY], readability changes

--- main.cpp
+++ main.cpp
@@ -1,32 +1,41 @@
-#include <iostream>
 #include <string>
+#include <iostream>
 #include <curl/curl.h>
 #include <regex>
 
+std::string replaceString(std::string x, std::string y, std::string z) {
+
+    std::string::size_type pos = 0;
+    while ((pos = x.find(y, pos)) != std::string::npos)
+    {
+        x.replace(pos, y.size(), z);
+        pos += 2;
+    }
+    return x;
+}
+
 size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
+
     data->append((char*) ptr, size * nmemb);
     return size * nmemb;
 }
 
 int main() {
 
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    auto curl = curl_easy_init();
-    std::string response_string;
-
     std::string searchTerm;
     std::getline(std::cin, searchTerm);
-    std::string::size_type size = 0;
 
-    while ((size = searchTerm.find(' ', size)) != std::string::npos)
-    {
-        searchTerm.replace(size, 1, "+");
-        size += 2;
-    }
+    searchTerm = replaceString(searchTerm, " ", "+");
 
-    std::string link = "https://lrclib.net/api/search?q=" + searchTerm;
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    auto curl = curl_easy_init();
+
+    std::string response_string;
 
     if (curl) {
+
+        const std::string link = "https://lrclib.net/api/search?q=" + searchTerm;
+
         curl_easy_setopt(curl, CURLOPT_URL, link.c_str());
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
         curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
@@ -54,14 +63,9 @@ int main() {
     std::regex_search(response_string, m, fmtend);
     response_string = m.prefix();
 
-    std::string::size_type pos = 0;
-
-    while ((pos = response_string.find("\\n", pos)) != std::string::npos)
-    {
-        response_string.replace(pos, 2, "\n");
-        pos += 2;
-    }
+    response_string = replaceString(response_string, "\\n", "\n");
 
     std::cout << response_string;
+
     return 0;
 }
